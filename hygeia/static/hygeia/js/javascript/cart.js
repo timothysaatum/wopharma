d429f706084f82e0fbb8bcb9dd133c98f49451f1
timothysaatum <tsaatum@outlook.com>
2023-09-13 19:27:52 +0100

    Updated project

---
+++ hygeia/static/hygeia/js/javascript/cart.js
@@ -0,0 +1,52 @@
+
+            const addCart = document.querySelectorAll(".order");
+
+			for(var i = 0; i < addCart.length; i++){
+				addCart[i].addEventListener('click', () => {
+					cartCount();
+				});
+			}
+
+			function cartCount() {
+				let productNumbers = localStorage.getItem("cartNumber");
+				productNumbers = parseInt(productNumbers);
+
+				if(productNumbers) {
+                  localStorage.setItem("cartNumber", productNumbers + 1);
+				  document.querySelector(".nav span").textContent = productNumbers + 1;
+				}
+				else {
+					localStorage.setItem("cartNumber", 1);
+					document.querySelector(".nav span").textContent = 1;
+				}
+
+				
+			}
+
+			function decrease() {
+				let productNumbers = localStorage.getItem("cartNumber");
+				productNumbers = parseInt(productNumbers);
+
+				if(productNumbers) {
+                  localStorage.setItem("cartNumber", productNumbers - 1);
+				  document.querySelector(".nav span").textContent = productNumbers - 1;
+				}
+				else {
+					localStorage.setItem("cartNumber", 0);
+					document.querySelector(".nav span").textContent = 0;
+				}
+
+				
+			}
+			
+            window.onload = function loadCartNumbers(){
+				let productNumbers = localStorage.getItem("cartNumber");
+				if(productNumbers){
+					document.querySelector('.nav span').textContent = productNumbers;
+				}
+			}
+		   
+			function deleteRow(o){
+				var p = o.parentNode.parentNode;
+				    p.parentNode.removeChild(p);
+			}
