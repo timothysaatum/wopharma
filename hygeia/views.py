09df7d8d935578396b7da4a21705de5373de267c
timothysaatum <tsaatum@outlook.com>
2023-09-24 09:54:52 +0100

    update

--- hygeia/views.py
+++ hygeia/views.py
@@ -28,5 +28,48 @@ def how_is_work(request):
 	return render(request, 'hygeia/how_is_works.html')
 
 def upload_prescription(request):
-	return render(request, 'hygeia/upload_prescription.html')	
+	return render(request, 'hygeia/upload_prescription.html')
+
+def herbal_medicine(request):
+	return render(request, 'hygeia/herbal_medicine.html')
+
+def sundries(request):
+	return render(request, 'hygeia/sundries.html')
+
+def cosmetics(request):
+	return render(request, 'hygeia/cosmetics.html')
+
+def vitamins(request):
+	return render(request, 'hygeia/vitamins.html')
+
+def snacks_confec(request):
+	return render(request, 'hygeia/snacks_confec.html')
+
+def baby_food(request):
+	return render(request, 'hygeia/baby_food.html')
+
+def sex_enhance(request):
+	return render(request, 'hygeia/sex_enhance.html')
+
+def pharmacies(request):
+	return render(request, 'hygeia/pharmacies.html')
+
+def book_consult(request):
+	return render(request, 'hygeia/book_consult.html')
+
+def drug_details_page(request):
+	return render(request, 'hygeia/drug_details_page.html')
+
+def checkout(request):
+	return render(request, 'hygeia/checkout.html')
+
+def cart_page(request):
+	return render(request, 'hygeia/cart_page.html')
+
+def consultation(request):
+	return render(request, 'hygeia/consultation.html')
+
+def specialist(request):
+	return render(request, 'hygeia/specialist.html')				
+
 	
